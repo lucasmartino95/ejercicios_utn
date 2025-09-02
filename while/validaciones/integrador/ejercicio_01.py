fd6001304af872472981761e6d75f8636b35f42c
Lucas Martino <lucasmartino2606@gmail.com>
2025-09-02 14:34:44 -0300

    Update validaciones/integrador

--- while/validaciones/integrador/ejercicio_01.py
+++ while/validaciones/integrador/ejercicio_01.py
@@ -16,7 +16,7 @@ while (estado_civil != "Soltero" and estado_civil != "Soltera") and \
         estado_civil = input("Ingresa tu estado civil "
         "(Soltero/a - Casado/a - Divorciado/a - Viudo/a): ")
 
-while numero_de_legajo < 1000:
+while numero_de_legajo < 1000 or numero_de_legajo >= 10000:
       print("No ingresaste un número de legajo válido")
       numero_de_legajo = int(input("Ingresa tu número de legajo (4 cifras): "))
 
