3c4099d7020542166a3e9e5180242c6329613fae
Lucas Martino <lucasmartino2606@gmail.com>
2025-09-09 17:47:07 -0300

    Update ejercicios/funciones

---
+++ funciones/ejercicio_03.py
@@ -0,0 +1,5 @@
+def devolver_cadena() -> str:
+    cadena: str = input("Escribe algo: ")
+    return cadena
+
+print(devolver_cadena())
---
+++ funciones/ejercicio_13.py
@@ -0,0 +1,20 @@
+def devolver_numero_entero() -> int:
+    numero: int = int(input("Ingresa un número entero: "))
+    return numero
+
+
+def devolver_numero_flotante() -> float:
+    numero: float = float(input("Ingresa un número con decimales: "))
+    return numero
+
+
+def devolver_cadena() -> str:
+    cadena: str = input("Escribe algo: ")
+    return cadena
+
+
+print(devolver_numero_entero())
+
+print(devolver_numero_flotante())
+
+print(devolver_cadena())
