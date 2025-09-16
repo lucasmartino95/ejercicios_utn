1dc79d43982c998b9f07bea9454bf66dc7d20ecc
Lucas Martino <lucasmartino2606@gmail.com>
2025-09-16 17:29:48 -0300

    Update code

--- funciones/ejercicio_13.py
+++ funciones/ejercicio_13.py
@@ -5,6 +5,8 @@ def devolver_numero_entero() -> int:
 
 def devolver_numero_flotante() -> float:
     numero: float = float(input("Ingresa un número con decimales: "))
+    while numero != type(float):
+        numero: float = float(input("Ingresa un número con decimales: "))
     return numero
 
 
