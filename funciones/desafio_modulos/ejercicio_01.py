4c3705b55e6a8905eddc2cf0ec0c9eb8298dfbec
Lucas Martino <lucasmartino2606@gmail.com>
2025-09-16 17:57:33 -0300

    Update programacion

---
+++ funciones/desafio_modulos/ejercicio_01.py
@@ -0,0 +1,32 @@
+def get_int(mensaje: str,
+            mensaje_error: str,
+            minimo: int,
+            maximo: int,
+            reintentos: int) -> int|None:
+    
+    print(mensaje)
+
+    numero: int = int(input("Ingresa un número: "))
+
+    limite: int = 0
+
+    bandera = True
+
+    while bandera:
+        if numero >= minimo and numero <= maximo:
+            dato_salida = numero
+            bandera = False
+            return dato_salida
+        else:
+            # Le resto 1 a reintentos para que se tome
+            # en cuenta el primer input que está fuera
+            # del while
+            if limite < (reintentos - 1):
+                print(mensaje_error)
+                limite += 1
+                numero: int = int(input("Ingresa un número: "))
+            else:
+                bandera = False
+                return None
+
+print(get_int("Pedir número", "Número inválido", 1, 10, 3))
