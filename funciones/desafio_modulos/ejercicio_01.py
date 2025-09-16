2b8640b2d2cd19f25720edb27651dec075a0d4c5
Lucas Martino <lucasmartino2606@gmail.com>
2025-09-16 18:00:15 -0300

    Update programacion

--- funciones/desafio_modulos/ejercicio_01.py
+++ funciones/desafio_modulos/ejercicio_01.py
@@ -28,5 +28,41 @@ def get_int(mensaje: str,
             else:
                 bandera = False
                 return None
+            
 
-print(get_int("Pedir número", "Número inválido", 1, 10, 3))
+def get_float(mensaje: str,
+            mensaje_error: str,
+            minimo: int,
+            maximo: int,
+            reintentos: int) -> float|None:
+    
+    print(mensaje)
+
+    numero: float = float(input("Ingresa un número: "))
+
+    limite: int = 0
+
+    bandera = True
+
+    while bandera:
+        if numero >= minimo and numero <= maximo:
+            dato_salida = numero
+            bandera = False
+            return dato_salida
+        else:
+            # Le resto 1 a reintentos para que se tome
+            # en cuenta el primer input que está fuera
+            # del while
+            if limite < (reintentos - 1):
+                print(mensaje_error)
+                limite += 1
+                numero: float = float(input("Ingresa un número: "))
+            else:
+                bandera = False
+                return None
+
+
+print(get_int("Pedir número entero", "Número inválido", 1, 10, 3))
+
+print(get_float("Pedir número con decimales", "Número inválido", 1, 10, 3))
+
