ab86f6c995a1894288ed1399fd70dc7c0219611b
Lucas Martino <lucasmartino2606@gmail.com>
2025-09-16 22:21:19 -0300

    Update programacion

--- funciones/ejercicio_13.py
+++ funciones/ejercicio_13.py
@@ -1,15 +1,21 @@
 def devolver_numero_entero() -> int:
+
     numero: int = int(input("Ingresa un número: "))
+
     return numero
 
 
 def devolver_numero_flotante() -> float:
+
     numero: float = float(input("Ingresa un número con decimales: "))
+
     return numero
 
 
 def devolver_cadena() -> str:
+
     cadena: str = input("Escribe algo: ")
+    
     return cadena
 
 
