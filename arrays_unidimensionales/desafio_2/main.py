44952c6548b5f3868396170a37811088f9407fb7
Lucas Martino <lucasmartino2606@gmail.com>
2025-09-27 16:25:17 -0300

    Update arrays

--- arrays_unidimensionales/desafio_2/main.py
+++ arrays_unidimensionales/desafio_2/main.py
@@ -2,3 +2,9 @@ from auxiliares import *
 
 mi_lista = crear_lista(1, 3)
 mi_lista_2 = crear_lista(2, 5)
+
+print(mi_lista, mi_lista_2)
+comparar_productos(mi_lista, mi_lista_2)
+comparar_productos_exclusivos(mi_lista, mi_lista_2)
+mostrar_catalogo_total(mi_lista, mi_lista_2)
+
