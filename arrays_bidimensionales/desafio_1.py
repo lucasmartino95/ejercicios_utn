b14aba1b29df0c1e5d6a6b70df19da67397ab865
Lucas Martino <lucasmartino2606@gmail.com>
2025-09-30 16:32:09 -0300

    Update arrays

--- arrays_bidimensionales/desafio_1.py
+++ arrays_bidimensionales/desafio_1.py
@@ -4,10 +4,11 @@ mi_matriz = [[1, 2, 3, 4],
 
 def validar_matriz(mi_matriz: list) -> bool:
 
-    numero_anterior = 0
+    numero = 0
 
     for i in range(len(mi_matriz)):
         for j in range(len(mi_matriz[i])):
+            
             if mi_matriz[i][j] >= 1:
                 continue
             else:
@@ -28,20 +29,7 @@ def mostrar_matriz(mi_matriz: list) -> None:
 
 
 if matriz_validada:
+    print("La matriz creada es: ")
     mostrar_matriz(mi_matriz)
 else:
     print("La matriz ingresada no es válida")
-
-
-def calcular_constante_magica(mi_matriz: list) -> bool:
-
-    suma_fila = 0
-
-    for i in range(len(mi_matriz)):
-        for j in range(len(mi_matriz[i])):
-            print(mi_matriz[i][j])
-            suma_fila += mi_matriz[i][j]
-
-
-
-calcular_constante_magica(mi_matriz)
