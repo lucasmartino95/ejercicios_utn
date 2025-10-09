eb8822b2dcbb2a8f76ecaca114b93878d04c17c4
Lucas Martino <lucasmartino2606@gmail.com>
2025-10-09 16:22:21 -0300

    Update arrays

--- arrays_bidimensionales/desafio_1.py
+++ arrays_bidimensionales/desafio_1.py
@@ -1,22 +1,20 @@
-mi_matriz = [[1, 2, 3],
-             [4, 5, 6],
-             [7, 8, 9]]
+mi_orden = int(input("Ingresa el orden de la matriz: "))
+
+
+def crear_matriz(orden: int) -> list:
+
+    mi_matriz = [[0] * orden] * orden 
+
+    return mi_matriz
 
 
 def mostrar_matriz(matriz: list) -> None:
-
     for i in range(len(matriz)):
         for j in range(len(matriz[i])):
             print(matriz[i][j], end=" ")
         print()
 
 
-def calcular_constante_magica(matriz: list) -> int:
 
-    n = len(matriz)
-    n = (n * ((n ** 2) + 1)) / 2
-    return n
-
-
-constante_magica = calcular_constante_magica(mi_matriz)
-print(constante_magica)
+mi_matriz = crear_matriz(mi_orden)
+mostrar_matriz(mi_matriz)
