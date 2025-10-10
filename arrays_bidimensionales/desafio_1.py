defc0d3add2552fdbd4a3caf7daf8c3ec537d2de
Lucas Martino <lucasmartino2606@gmail.com>
2025-10-09 21:13:17 -0300

    Update arrays bidimensionales

--- arrays_bidimensionales/desafio_1.py
+++ arrays_bidimensionales/desafio_1.py
@@ -3,7 +3,10 @@ mi_orden = int(input("Ingresa el orden de la matriz: "))
 
 def crear_matriz(orden: int) -> list:
 
-    mi_matriz = [[0] * orden] * orden 
+    mi_matriz = [0] * orden
+
+    for i in range(orden):
+        mi_matriz[i] = [0] * orden
 
     return mi_matriz
 
@@ -15,5 +18,20 @@ def mostrar_matriz(matriz: list) -> None:
         print()
 
 
+def cargar_matriz(matriz: list) -> list:
+
+    for i in range(len(matriz)):
+        print(f"Fila {i}")
+        for j in range(len(matriz[i])):
+            print(f"Columna {j}")
+            matriz[i][j] = int(input("Ingresa un número: "))
+            while matriz[i][j] < 1:
+                print("Debes ingresar números mayores que 1")
+                matriz[i][j] = int(input("Ingresa un número: "))
+
+    return matriz
+
+
 mi_matriz = crear_matriz(mi_orden)
+mi_matriz = cargar_matriz(mi_matriz)
 mostrar_matriz(mi_matriz)
